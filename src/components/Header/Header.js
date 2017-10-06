79d2bee79638d0b74223e219fba64fafffe57268
chrisjxn <usjacksons@gmail.com>
2017-10-06 13:15:05 -0600

    main layout with header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+    return (
+        <div className="header_wrapper">
+            <div className="main_nav">
+                <ul>
+                    <li>Contact</li>
+                    <li>About</li>
+                    <li>Store</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
